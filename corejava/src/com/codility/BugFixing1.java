8119398920035c60f8be21d3d390a76ad53e197d
Kiruba <kiruba98@gmail.com>
2020-12-18 12:05:51 +0530

    conflict commit in second

--- corejava/src/com/codility/BugFixing1.java
+++ corejava/src/com/codility/BugFixing1.java
@@ -29,6 +29,7 @@ public class BugFixing1 {
    	System.out.println(new BugFixing1().solution("parameter"));
    	System.out.println(new BugFixing1().solution("ok"));
    	System.out.println(new BugFixing1().solution("kk"));
+   	System.out.println(new BugFixing1().solution("second change"));
    
    	
    }
